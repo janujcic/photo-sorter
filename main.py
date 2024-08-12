59ea9362b84c8942ed77031c7b272a7cc6433a44
jujcic <jan.ujcic@parsek.com>
2024-08-12 11:24:47 +0200

    Sorting to specific folders added.

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ import imagehash
 from collections import defaultdict
 import re
 import reverse_geocode
+import ffmpeg
 
 def extract_exif(file_path):
     file_extension = os.path.splitext(file_path)[1].lower()
 
-    if file_extension == '.heic':
+    if file_extension == '.heic' or file_extension == '.HEIC':
         # Use pillow-heif for HEIC format
         heif_image = pillow_heif.open_heif(file_path)
         exif_data = heif_image.getexif()
@@ -144,21 +145,33 @@ def verify_duplicates(duplicates_list, hash_map):
     return duplicates_list, hash_not_found
 
 
-def move_files(source_folder, file_names, duplicates_folder):
+def move_files(source_folder, file_names, target_folder):
     for file_name in file_names:
         file_path = os.path.join(source_folder, file_name)
+        destination_path = os.path.join(target_folder, file_name)
+
+        if os.path.exists(destination_path):
+            print(f"File {file_name} already exists in {target_folder}. Skipping...")
+            continue
+
         try:
-            shutil.move(file_path, duplicates_folder)
-            print(f"Moved duplicate: {file_path}")
+            shutil.move(file_path, target_folder)
+            print(f"Moved file: {file_path}")
         except Exception as e:
             print(f"Error moving {file_path}: {e}")
 
 def copy_files(source_folder, file_names, target_folder):
     for file_name in file_names:
         file_path = os.path.join(source_folder, file_name)
+        destination_path = os.path.join(target_folder, file_name)
+
+        if os.path.exists(destination_path):
+            print(f"File {file_name} already exists in {destination_path}. Skipping...")
+            continue
+
         try:
-            shutil.copy(file_path, target_folder)
-            print(f"Moved duplicate: {file_path}")
+            shutil.copy(file_path, destination_path)
+            print(f"Copied file: {file_path}")
         except Exception as e:
             print(f"Error moving {file_path}: {e}")
 
@@ -204,7 +217,10 @@ def sort_duplicates(source_folder, manual_check_duplicates_folder, broken_photos
 
 
 def create_file_name(image_date, image_time, file_geo_data):
-    return file_geo_data["country_code"] + "_" + image_date + "_" + image_time
+    if (file_geo_data != ""):
+        return file_geo_data["country_code"] + "_" + image_date + "_" + image_time
+    else:
+        return image_date + "_" + image_time
 
 def rename_file(path, original_name, new_name):
     file_extension = original_name.split(".")[1]
@@ -217,40 +233,104 @@ def rename_file(path, original_name, new_name):
 
     print(original_name + " renamed to -> " + new_name)
 
+
+def rename_folder(path, original_name, new_name):
+    old_path = os.path.join(path, original_name)
+    new_path = os.path.join(path, new_name)
+    os.rename(old_path, new_path)
+    print(original_name + " folder renamed to -> " + new_name)
+
+def get_media_created(video_path):
+    print(video_path)
+    probe = ffmpeg.probe(video_path)
+    if 'format' in probe and 'tags' in probe['format']:
+        media_created = probe['format']['tags'].get('creation_time')
+        return media_created
+    else:
+        return None
+    
+def create_datetime_and_country_folder(parent_folder_path, datetime, country):
+    folder_name = datetime + "_" + country
+    folder_path = os.path.join(parent_folder_path, folder_name)
+    os.mkdir(folder_path)
+    print("New folder created in path: " + folder_path)
+
+
+def update_datetime_and_country_folder(parent_folder_path, original_folder_name, country):
+    folder_parts = original_folder_name.split("_")
+    if (len(folder_parts) > 2):
+        countries = folder_parts[-1].split(",")
+        if country in countries:
+            return
+        else:
+            new_folder_name = original_folder_name + "," + country
+    else:
+        new_folder_name = original_folder_name + "_" + country
+    rename_folder(parent_folder_path, original_folder_name, new_folder_name)
+                    
+
+def move_file_to_specific_datetime_folder(source_folder, parent_target_folder, file_name, file_date, file_country):
+    file_year_month = file_date[0:6]
+    folder_names = [f for f in os.listdir(parent_target_folder) if os.path.isdir(os.path.join(parent_target_folder, f))]
+    if (len(folder_names) > 0):
+        for folder_name in folder_names:
+            folder_year_month = folder_name.split("_")[0] + folder_name.split("_")[1]
+            if folder_year_month == file_year_month:
+                target_folder = os.path.join(parent_target_folder, folder_name)
+                move_files(source_folder, [file_name], target_folder)
+                update_datetime_and_country_folder(parent_target_folder, folder_name, file_country)
+                return
+    
+    folder_date = file_date[0:4] + "_" +  file_date[4:6]
+    folder_name = file_date[0:4] + "_" +  file_date[4:6] + "_" + file_country
+    create_datetime_and_country_folder(parent_target_folder, folder_date, file_country)
+    target_folder = os.path.join(parent_target_folder, folder_name)
+    move_files(source_folder, [file_name], target_folder)
+              
+
 def sort_pictures_into_folders(source_folder, target_folder):
 
     file_names = [f for f in os.listdir(source_folder) if os.path.isfile(os.path.join(source_folder, f))]
 
+    i = 0
     for file_name in file_names:
         print(file_name)
 
         try:
             check_file_name_changed = file_name.split("_")
             if len(check_file_name_changed) == 3:
+                file_split = file_name.split("_")
+                file_date = file_split[1]
+                file_country = file_split[0]
+                move_file_to_specific_datetime_folder(source_folder, target_folder, file_name, file_date, file_country)
                 continue
 
             file_path = os.path.join(source_folder, file_name)
             
             image_data = extract_exif(file_path)
-            image_datetime = image_data["DateTime"]
-            image_date = (image_datetime.split(" "))[0].replace(":", "")
-            image_time = (image_datetime.split(" "))[1].replace(":", "")
+            if (image_data == None):
+                print("Skipping since metadata is not found")
+                #new_file_name = create_file_name(file_date, file_time, "")
+            
+            else:
+                image_datetime = image_data["DateTime"]
+                file_date = (image_datetime.split(" "))[0].replace(":", "")
+                file_time = (image_datetime.split(" "))[1].replace(":", "")
+                image_gps = extract_image_gps_info(file_path)
+                print("File GPS data: " + str(image_gps))
 
-            image_gps = extract_image_gps_info(file_path)
-            print("File GPS data: " + str(image_gps))
+                image_geo_data = get_data_from_geocode(image_gps)
+                print(image_geo_data)
 
-            image_geo_data = get_data_from_geocode(image_gps)
-            print(image_geo_data)
+                new_file_name = create_file_name(file_date, file_time, image_geo_data)
 
-            new_image_name = create_file_name(image_date, image_time, image_geo_data)
-
-            rename_file(source_folder, file_name, new_image_name)
+            
+            rename_file(source_folder, file_name, new_file_name)
+            move_file_to_specific_datetime_folder(source_folder, target_folder, new_file_name, file_date, file_country)
 
         except Exception as e:
             print(f"Error processing file {file_name}: {e}") 
         
-
-
 
 if __name__ == "__main__":
     # Load config
@@ -279,3 +359,5 @@ if __name__ == "__main__":
     #sort_duplicates(source_folder, manual_check_duplicates_folder, broken_photos_folder, duplicate_photos_folder)
 
     sort_pictures_into_folders(source_folder, sorted_photos_folder)
+
+    #print(get_media_created(example_file_path))
